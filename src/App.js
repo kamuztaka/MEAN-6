46edf58fe348654710f6fb5f40cc7a8f5b18f077
kamuztaka <bschultzhenry@yahoo.com>
2021-11-06 14:43:52 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const Me = (props) => {
+  return <li>I am a {props.ako}</li>;
+};
+
+const App = () => {
+  const mycv = ["name", "address", "email address", "course", "phone number"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ color: "red" }}>My CV</h1>
+      <h2>Okey...</h2>
+      <ul>
+        {mycv.map((mycv) => {
+          return <Me ako={mycv} />;
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
